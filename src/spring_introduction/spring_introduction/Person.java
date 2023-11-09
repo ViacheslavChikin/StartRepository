c384c6944166933dbcc371b1be21aea943722074
Viasheslav Chikin <slv3211@gmail.com>
2023-11-09 23:50:20 +0300

    09.11 add @auto

---
+++ src/spring_introduction/spring_introduction/Person.java
@@ -0,0 +1,37 @@
+package spring_introduction;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+    @Value("34")
+    int age;
+    @Value("${person.name}")
+    String name;
+   //
+    Pat pat;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Pat getPat() {
+        return pat;
+    }
+
+    @Autowired
+    public Person(Pat pat) {
+//        this.name = this.name;
+//        this.age = this.age;
+        this.pat = pat;
+        System.out.println("person tut!");
+    }
+}
--- src/spring_introduction/spring_introduction/Test.java
+++ src/spring_introduction/spring_introduction/Test.java
@@ -1,11 +1,16 @@
 package spring_introduction;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import spring_introduction.Dog;
 import spring_introduction.Pat;
 
 public class Test {
     public static void main(String[] args) {
-        Pat pat = new Dog();
-        pat.say();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
+        Person person = context.getBean("person", Person.class);
+        person.pat.say();
+    System.out.println(person.getAge());
+        System.out.println(person.name);
+        context.close();
     }
 }
