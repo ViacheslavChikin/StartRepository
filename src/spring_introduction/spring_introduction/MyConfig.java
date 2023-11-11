293e59cc806d1d07eafd1985f9648997b8c6f281
Viasheslav Chikin <slv3211@gmail.com>
2023-11-11 21:25:32 +0300

    11.11 ii

---
+++ src/spring_introduction/spring_introduction/MyConfig.java
@@ -0,0 +1,11 @@
+package spring_introduction;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:properties")
+@ComponentScan("spring_introduction")
+public class MyConfig {
+}
---
+++ src/spring_introduction/spring_introduction/Test6.java
@@ -0,0 +1,15 @@
+package spring_introduction;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.text.Annotation;
+
+public class Test6 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        Person person = context.getBean("person", Person.class);
+        person.pat.say();
+        System.out.println(person.getName());
+
+    }
+}
