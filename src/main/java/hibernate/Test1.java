53eef5aabae82f97694df8768472043925e93188
Viasheslav Chikin <slv3211@gmail.com>
2023-11-16 22:16:04 +0300

    16.11 GoodStart MH

---
+++ src/main/java/hibernate/Test1.java
@@ -0,0 +1,23 @@
+package hibernate;
+
+import hibernate.entity.Employee;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.util.Properties;
+
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        System.out.println();
+        SessionFactory sessionFactory =  new Configuration()
+                .configure()
+                .buildSessionFactory();
+       SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+               .addAnnotatedClass(Employee.class)
+               .buildSessionFactory();
+
+    }
+}
--- src/zaur/Test.java
+++ src/zaur/Test.java
@@ -5,7 +5,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext2.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext
+                ("applicationContext2.xml");
         Person person = context.getBean("personBean", Person.class);
 
         person.pat.say();
