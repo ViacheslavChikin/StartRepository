d06806300a64fcf42085696dc20a3942422e8859
Viasheslav Chikin <slv3211@gmail.com>
2023-11-18 00:26:35 +0300

    18.11 Add try

--- src/main/java/hibernate/Test1.java
+++ src/main/java/hibernate/Test1.java
@@ -1,10 +1,13 @@
 package hibernate;
 
+
 import hibernate.entity.Employee;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 
+import java.util.List;
 import java.util.Properties;
 
 
@@ -14,9 +17,23 @@ public class Test1 {
 //        SessionFactory sessionFactory =  new Configuration()
 //                .configure()
 //                .buildSessionFactory();
-       SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
-               .addAnnotatedClass(Employee.class)
-               .buildSessionFactory();
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+//
+ //       Session session = factory.getCurrentSession();
+//        Employee employee = new Employee("Cfif", "Gtnhjd", "ше", 2000);
+//        session.beginTransaction();
+//        session.save(employee);
+//        session.getTransaction().commit();
 
+    //    factory.close();
+        List<Employee> employees;
+        try (Session session1 = factory.openSession()) {
+            employees = session1.createQuery("from Employee", Employee.class).list();
+        }
+        for (Employee employee1 : employees) {
+            System.out.println(employee1);
+        }
     }
 }
